fbdb65fc497151d8823f7fbd33dca52393616cb0
WebMakaka <marley.nodejs@gmail.com>
2020-05-09 10:15:20 +0300

    10. Testing Isolated Microservices | 05. A Few Dependencies

--- 10_Testing_Isolated_Microservices.md
+++ 10_Testing_Isolated_Microservices.md
@@ -8,6 +8,40 @@
 
 <br/>
 
+### 02. Testing Goals
+
+<br/>
+
+![Application](/img/pic-10-01.png?raw=true)
+
+<br/>
+
+![Application](/img/pic-10-02.png?raw=true)
+
+<br/>
+
+![Application](/img/pic-10-03.png?raw=true)
+
+<br/>
+
+### 03. Testing Architecture
+
+<br/>
+
+![Application](/img/pic-10-04.png?raw=true)
+
+<br/>
+
+### 04. Index to App Refactor
+
+<br/>
+
+### 05. A Few Dependencies
+
+    $ npm install --save-dev @types/jest @types/supertest jest ts-jest supertest mongodb-memory-server
+
+<br/>
+
 ---
 
 <br/>
--- 10_Testing_Isolated_Microservices/app/auth/src/index.ts
+++ 10_Testing_Isolated_Microservices/app/auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
